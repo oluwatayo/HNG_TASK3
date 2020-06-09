47e285a237f56ee87ba425dae6845827fc914320
oluwatayo <oluwatayoadegboye@gmail.com>
2020-06-09 16:02:49 +0100

    use flask_restplus to generate swagger doc

--- sms.py
+++ sms.py
@@ -1,68 +1,106 @@
+import os
 from flask import Flask, request, redirect, make_response, redirect, url_for
+from flask_restplus import Api, Resource, fields
 from random import randint
 from twilio_api import get_twilio_criteria, twilio_responder, twilio_sender
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return redirect(url_for("login"))
+flask_app = Flask(__name__)
+flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app)
+app = Api(app=flask_app)
+
+
+
+
+sms_name_space = app.namespace('', description='SMS APIs')
+
+
+
 
 """
 This is the details route. Enter the endpoint to get your details.
 You can change the client_number to your number if it's registered on the current account
 """
-@app.route("/details", methods=["GET"])
-def login():
-    criteria = get_twilio_criteria()
-    #All this does is return JSON for you to use as headers
-    return {"save these as headers, you can substitute the values with yours": criteria}
+@sms_name_space.route("/details")
+class Login(Resource):
+    def get(self):
+        """All this does is return JSON for you to use as headers"""
+        criteria = get_twilio_criteria()
+        # All this does is return JSON for you to use as headers
+        return {"save these as headers, you can substitute the values with yours": criteria}
 
 
 """
 This is the incoming SMS endpoint, it listens for your SMS and replies accordingly if your number is registered.
 This endpoint is best left untouched if you're not developing
 """
-@app.route("/sms", methods=['POST'])
-def incoming_sms():
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our number
-    body = request.values.get('Body', None)
 
-    # Determine the right reply for this message
-    if body.lower() in ("hello", "hi"):
-        message = "Hi from Team-Titans! Send 'CHECK BALANCE' or '1' to see your balance. \nSend 'BYE' or '2' for a goodbye."
 
-    elif body.lower() in ("check balance", '1'):
-        message = f"Your balance is {randint(1000,99999)}"
+@sms_name_space.route("/sms")
+class IncomingSms(Resource):
+    def post(self):
+        """Send a dynamic reply to an incoming text message"""
+        # Get the message the user sent our number
+        body = request.values.get("Body")
+        print(body)
 
-    elif body.lower() in ('bye', "2"):
-        message = "Goodbye"
+        # Determine the right reply for this message
+        if body.lower() in ("hello", "hi"):
+            message = "Hi from Team-Titans! Send 'CHECK BALANCE' or '1' to see your balance. \nSend 'BYE' or '2' for a goodbye."
 
-    elif body:
-        message = "Invalid message, Type 'HELLO' or 'HI' for a tip"
+        elif body.lower() in ("check balance", '1'):
+            message = f"Your balance is {randint(1000,99999)}"
 
-    #Call the Twilio Responder passing the message to it
-    response = twilio_responder(message)
-    return response
+        elif body.lower() in ('bye', "2"):
+            message = "Goodbye"
+
+        elif body:
+            message = "Invalid message, Type 'HELLO' or 'HI' for a tip"
+
+        # Call the Twilio Responder passing the message to it
+        response = twilio_responder(message)
+        return response
+
 
 """
 This endpoint is for sending an SMS to your number using your client app.
 Using the headers already set from the "/details" endpoint, all you have to do is send a JSON containing the text body and the client number if you wish to change that
 It results in an SMS to the client number if registered with the current account
 """
-@app.route("/send", methods=["GET", "POST"])
-def outgoing_sms():
-    #Checking the request method to proceed if POST
-    if request.method == "POST":
-        #Calls the function for sending the text
+
+send_data = sms_name_space.model("Json data to send when calling the send endpoint",
+                              {
+                                  "sender":
+                                  fields.String(
+                                      description="Sender", required=True),
+                                  "receiver":
+                                  fields.String(
+                                      description="Receiver", required=True),
+                                  "text":
+                                  fields.String(
+                                      description="text", required=True)
+                              }
+                              )
+#parser to add required headers to the request
+parser = sms_name_space.parser()
+parser.add_argument('account_sid', location='headers')
+parser.add_argument('receiver', location='headers')
+parser.add_argument('sender', location='headers')
+parser.add_argument('auth_token', location='headers')
+
+@sms_name_space.route("/send", endpoint="send")
+class OutgoingSms(Resource):
+    @sms_name_space.expect(send_data, parser)
+    def post(self):
+        """Checking the request method to proceed if POST"""
         response = twilio_sender(request)
         return response
 
-    #return JSON with criteria to fill
-    return { "Fill out the text body and client number if empty and submit as JSON": dict(sender=request.headers.get('sender'),
-                                                                                          reciever=request.headers.get("receiver"),
-                                                                                          text="")
-            }
-        
-
+    def get(self):
+        """return JSON with criteria to fill"""
+        return {"Fill out the text body and client number if empty and submit as JSON": dict(sender=request.headers.get('sender'),
+                                                                                             reciever=request.headers.get(
+            "receiver"),
+            text="")
+        }
